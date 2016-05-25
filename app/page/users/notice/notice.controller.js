3e447b0c8f2ed5461539b102bcd213fe8a40cb1b
向问天 <xiangsongtao@xiangwentiandeMacBook-Pro.local>
2016-05-25 20:11:51 +0800

    增加消息

--- app/page/users/notice/notice.controller.js
+++ app/page/users/notice/notice.controller.js
@@ -3,71 +3,93 @@
  * 系统消息 controller
  */
 (function () {
-  angular.module('smartac.page')
-      .controller('noticeCtrl', ['$scope','$ionicPopup',function ($scope,$ionicPopup) {
+    angular.module('smartac.page')
+        .controller('noticeCtrl', ['$scope', '$ionicPopup', '$getMessage', '$ionicLoading', '$changeMessageStatus', function ($scope, $ionicPopup, $getMessage, $ionicLoading, $changeMessageStatus) {
 
-        $scope.data = {
-          showDelete: false
-        };
-
-        $scope.delete = function(item) {
-          $scope.items.splice($scope.items.indexOf(item), 1);
-        };
-
-        $scope.showNoticeInfo = function(item) {
-          var alertPopup = $ionicPopup.show({
-            title: item.title, // String. The title of the popup.
-            cssClass: 'noticePopup', // String, The custom CSS class name
-            subTitle: '', // String (optional). The sub-title of the popup.
-            template: item.content, // String (optional). The html template to place in the popup body.
-            //templateUrl: '', // String (optional). The URL of an html template to place in the popup   body.
-            //scope: $scope, // Scope (optional). A scope to link to the popup content.
-            buttons: [{ // Array[Object] (optional). Buttons to place in the popup footer.
-              text: '确定',
-              type: 'noticePopupBtn',
-              onTap: function(e) {
-                // e.preventDefault() will stop the popup from closing when tapped.
-                //e.preventDefault();
-              }
-            }]
-          });
-          //alertPopup.then(function(res) {
-          //  console.log('Thank you for not eating my delicious ice cream cone');
-          //});
-        };
-
-        $scope.items = [
-          {
-            id: 0,
-            title:"会员卡消息",
-            content:"恭喜您!您的会员卡等级已提升至VIP!0",
-            time:"2016年3月12日"
-          },
-          {
-            id: 1,
-            title:"系统消息",
-            content:"vivoCity将在一周后进行升级,请及时下载更新!",
-            time:"2016年3月18日"
-          }
-          ,
-          {
-            id: 2,
-            title:"临时消息",
-            content:"您的账号异地登陆,希望您注意您的账号安全!",
-            time:"2016年3月19日"
-          }
-          ,
-          {
-            id: 3,
-            title:"积分消息",
-            content:"黄记煌积分满300送300,从2016年3月30日起,截止到4月30日,先到先得!机不可失失不再来啊,亲们,要记住日期哈,一般我家的优惠券是不会这么放血的,现在老板都看不下去了加我撤消息,不过我还是偷偷的放出来了,亲么,我容易吗?不都是为了亲们能吃到实惠的黄记煌吗!呜呜呜呜呜....",
-            time:"2016年3月26日"
-          }
-        ];
+            /**
+             * 获取列表
+             * */
+            var unReadNum = 0;
+            $ionicLoading.show();
+            $getMessage({
+                "method": "query",
+                "querytype": "main",//count
+                "message": {
+                    "statuscode": null//#状态：0未读/1已读/2删除
+                },
+                "dsc": {
+                    "order_by": "createdtime",
+                    "order_type": "desc",
+                    "page_index": 1,
+                    "page_size": 999
+                }
+            }).then(function (data) {
+                for(var i= 0,len = data.length;len>i;i++){
+                    if(!data[i].statuscode){
+                        unReadNum++;
+                    }
+                }
+                console.log('unReadNum')
+                console.log(unReadNum)
+                $scope.items = data;
+            }).finally(function () {
+                $ionicLoading.hide();
+            });
 
 
 
+            $scope.data = {
+                showDelete: false
+            };
 
-      }]);
+            /**
+             * 消息删除
+             * */
+            $scope.delete = function (item) {
+                return $changeMessageStatus({
+                    "method": "update",
+                    "message": {
+                        "id": item.id,
+                        "statuscode": 2
+                    }
+                }).then(function () {
+                    //更新本地列表
+                    $scope.items.splice($scope.items.indexOf(item), 1);
+                })
+
+            };
+
+            /**
+             * 消息显示
+             * */
+            $scope.showNoticeInfo = function (item) {
+                $ionicPopup.show({
+                    title: item.title,
+                    cssClass: 'noticePopup',
+                    subTitle: '',
+                    template: item.content,
+                    buttons: [{
+                        text: '确定',
+                        type: 'noticePopupBtn',
+                        onTap: function (e) {
+                        }
+                    }]
+                });
+                //列表更新
+                if (!item.statuscode) {
+                    return $changeMessageStatus({
+                        "method": "update",
+                        "message": {
+                            "id": item.id,
+                            "statuscode": 1
+                        }
+                    }).then(function () {
+                        //更新本地列表
+                        $scope.items[$scope.items.indexOf(item)].statuscode = 1;
+                    })
+                }
+            };
+
+        }]);
 
 })();
