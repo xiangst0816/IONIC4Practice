4b8741e01463838d4a88765e953cbc3bbf34d77d
向松涛 <280304286@163.com>
2016-05-30 20:43:01 +0800

    首次进入的引导页

--- app/common/directives/firstInGuide.directive.js
+++ app/common/directives/firstInGuide.directive.js
@@ -1,33 +1,77 @@
 /**
  * Created by xiangsongtao on 16/5/25.
+ * 设置app首次进入时,首页silder页和使用引导页
+ * 只在home页显示,如果进入后跳转到其余页则隐藏,但是返回home还是会显示
+ *
  */
 (function () {
     angular.module('smartac.page')
-        .directive('firstInGuide', ['$ionicBackdrop', '$localStorage', function ($ionicBackdrop, $localStorage) {
+        .directive('firstInGuide', ['$localStorage', '$timeout', '$state','$ionicSlideBoxDelegate', function ($localStorage, $timeout, $state,$ionicSlideBoxDelegate) {
             return {
-                restrict: 'A',
-                compile: function (ele) {
+                restrict: 'E',
+                // template: '<div id="firstGuideBox" class="firstGuideBox"><div class="userGuideBox"><img src="img/loading/usercenterGuide.png" alt=""></div><div class="planetGuideBox"><img src="img/loading/planetGuide.png" alt=""></div>',
+                templateUrl:'tpl/firstInGuide.html',
+                replace: true,
+                compile:function ($element) {
                     var guideInfo = $localStorage.guideInfo;
-                    if (!guideInfo || guideInfo.isFirstTime) {
-                        var $body = angular.element(document.getElementsByTagName('body')[0]);
-                        $body.append('<div id="firstGuideBox" first-in class="firstGuideBox"><div class="userGuideBox"><img src="img/loading/usercenterGuide.png" alt=""></div><div class="planetGuideBox"><img src="img/loading/planetGuide.png" alt=""></div>');
-                        $ionicBackdrop.retain();
+                    $timeout(function () {
+                        if ($state.is("home") && (!guideInfo || guideInfo.isFirstTime)) {
+                            $element.addClass("beforeActive active");
+                        }
+                    },0,false);
+                },
+                controller: ['$scope', '$element', function ($scope, $element) {
+                    $scope.$on("$stateChangeSuccess", function () {
+                        //必须放到异步队列中
+                        $timeout(function () {
+                            var guideInfo = $localStorage.guideInfo;
+                            //只在主页显示
+                            if ($state.is("home") && (!guideInfo || guideInfo.isFirstTime)) {
+                                //满足显示条件
+                                // $element.addClass("beforeActive active");
+                                var $userSilder = $element.children("#userSilder");
+                                $scope.silderEnd = function () {
+                                    removeObj($userSilder);
+                                };
+                                $scope.closeGuide = function () {
+                                    removeObj($element);
+                                    $timeout(function () {
+                                        $element.remove();
+                                    }, 300, false);
+                                    //设置已读
+                                    $localStorage.guideInfo = {
+                                        isFirstTime: false
+                                    };
+                                };
+                            } else {
+                                removeObj($element);
+                            }
+                        }, 0, false);
+                    });
 
-                        //记录
-                        $localStorage.guideInfo = {
-                            isFirstTime:false
-                        };
+                    /**
+                     * 设置背景颜色
+                     * */
+                    $scope.onSlideChange = function ($event) {
+                        var currentIndex = $ionicSlideBoxDelegate.currentIndex();
+                        var $userSilderBox = angular.element(document.getElementById('userSilderBox'));
+                        if(currentIndex == 0){
+                            $userSilderBox.css({"background": "#1898d2"})
+                        }else if(currentIndex == 2){
+                            $userSilderBox.css({"background": "#e8ad3f"})
+                        }
+                    };
 
-                        //移除
-                        var $showShareDark = angular.element(document.getElementById('firstGuideBox'));
-                        $showShareDark.on("touchend", function () {
-                            $showShareDark.remove();
-                            // 必须release两次
-                            $ionicBackdrop.release();
-                            $ionicBackdrop.release();
-                        });
+                    /**
+                     * 移除active类
+                     * */
+                    function removeObj($obj) {
+                        $obj.removeClass("active");
+                        $timeout(function () {
+                            $obj.removeClass("beforeActive");
+                        }, 300, false);
                     }
-                }
+                }]
             };
         }])
 })();
